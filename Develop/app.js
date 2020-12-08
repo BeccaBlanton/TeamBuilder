0e11c11878b5866e8183e2168df59498ccc690cf
Rebecca Blanton <beccaablanton@gmail.com>
2020-12-07 18:20:11 -0800

    added to employeesQuestions function to create objects for each user with answers from prompts, passed through to use in htmlRenderer.js

--- Develop/app.js
+++ Develop/app.js
@@ -9,27 +9,10 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-const engineerPrompt = {
-    type: 'input',
-    name: 'github',
-    message: 'what is your github username?'
-};
-
-const internPrompt = {
-    type: 'input',
-    name: 'school',
-    message: 'What school do you go to?'
-};
-
-const managerPrompt = {
-    type: 'input',
-    name: 'officeNumber',
-    message: 'What is your Office Number?'
-}
-
-function employeeQuestions(){
-inquirer.prompt([
+//array to put in all user inputted emplyee objects
+const employees = []
+//first 4 questions using inquirer to get emplyee info
+const initialPrompts = [
     {
         type: 'input',
        message: "What's your name?",
@@ -50,28 +33,58 @@ inquirer.prompt([
     name: 'jobtitle',
     message: 'Whats your job title?',
     choices: ['Engineer', 'Intern', 'Manager']
+}]
+//job type specific questions based on job title listed
+const engineerPrompt = {
+    type: 'input',
+    name: 'github',
+    message: 'what is your github username?'
+};
+
+const internPrompt = {
+    type: 'input',
+    name: 'school',
+    message: 'What school do you go to?'
+};
+
+const managerPrompt = {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is your Office Number?'
 }
-]).then((answers) =>{
+
+//function to ask questions and create emplyee objects, then pushed into employees array
+function employeeQuestions(){
+inquirer.prompt(initialPrompts).then(function(answers) {
     if (answers.jobtitle === 'Engineer'){
         inquirer.prompt(engineerPrompt).then(()=>{
-            moreEmployees();
+           let employee = new Engineer(answers.name, answers.id, answers.email, answers.github)
+            employees.push(employee)
+            console.log(employees)
+            moreEmployees(employees);
         })
         
     } else if(answers.jobtitle === 'Intern'){
        inquirer.prompt(internPrompt).then(()=>{
-        moreEmployees();
+           let employee = new Intern(answers.name, answers.id, answers.email, answers.school)
+           employees.push(employee)
+           console.log(employees)
+        moreEmployees(employees);
     })
     } else {
         inquirer.prompt(managerPrompt).then(()=>{
-            moreEmployees();
+            let employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            employees.push(employee)
+            console.log(employees)
+            moreEmployees(employees);
         })
     }
    
 })
 }
 
-
-function moreEmployees(){
+//function to rerun questions until done inputting all employees
+function moreEmployees(employees){
     inquirer.prompt({
         type: 'confirm',
         name: 'continue',
@@ -80,15 +93,24 @@ function moreEmployees(){
         if(answers.continue){
             employeeQuestions(); 
          }else{
-          render()
+          render(employees)
+
          }
     })
     
 }
+//function to make a final html file that will go into output folder
+//function makeFile(){
+    //fs.writeFile(outputPath, html, (err) => err ? console.log(err) : console.log('yay'))
+
+   // }
+
+
 employeeQuestions()
 
+
 //√ Write code to use inquirer to gather information about the development team members,
-//√ and to create objects for each team member (using the correct classes as blueprints!)
+//and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
